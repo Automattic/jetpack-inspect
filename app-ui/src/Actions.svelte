e3d92e20a729eb41411c29dd21c255749f627cc6
pyronaur <hi@pyronaur.com>
2022-07-04 15:07:14 +0300

    Add Actions placeholder

---
+++ app-ui/src/Actions.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	function watch() {
+		console.log("watch");
+	}
+
+	function clear() {
+		console.log("clear");
+	}
+</script>
+
+<div class="actions">
+	<h2>Actions</h2>
+	<hr />
+	<button class="button button-primary" on:click|preventDefault={watch}>
+		Watch
+	</button>
+
+	<button class="button button-primary" on:click|preventDefault={clear}>
+		Clear
+	</button>
+</div>
