2d6b2d43e2232f9d60744bef659ccefeb7ced897
pyronaur <hi@pyronaur.com>
2022-07-15 12:11:59 +0300

    Show args used that produced the outbound errror

--- app-ui/src/Dashboard/Log/OutboundErrorDetails.svelte
+++ app-ui/src/Dashboard/Log/OutboundErrorDetails.svelte
@@ -1,23 +1,37 @@
 <script type="ts">
 	import type { OutboundRequestError } from "@src/utils/Validator";
-	import PrettyJson from "./PrettyJSON.svelte";
+	import PrettyJSON from "@src/Dashboard/Log/PrettyJSON.svelte";
+	import Tabs from "@src/Components/Tabs/Tabs.svelte";
+	import TabList from "@src/Components/Tabs/TabList.svelte";
+	import TabPanel from "@src/Components/Tabs/TabPanel.svelte";
+	import Tab from "@src/Components/Tabs/Tab.svelte";
 
 	export let details: OutboundRequestError;
 </script>
 
-<div>
-	{#each Object.entries(details.error.errors) as [error, name]}
-		<h4>{name}</h4>
-		<p>{error}</p>
-	{/each}
-	{#if details.error.error_data}
-		<PrettyJson data={details.error.error_data} />
-	{/if}
-</div>
+<Tabs>
+	<TabList>
+		<Tab>Errors</Tab>
+		<Tab>Args</Tab>
+	</TabList>
 
+	<TabPanel>
+		{#each Object.entries(details.error.errors) as [error, name]}
+			<h4>{name}</h4>
+			<p>{error}</p>
+		{/each}
+		{#if details.error.error_data}
+			<PrettyJSON data={details.error.error_data} />
+		{/if}
+	</TabPanel>
+
+	<TabPanel>
+		<PrettyJSON data={details.args} />
+	</TabPanel>
+</Tabs>
 
 <style>
-	div {
-		padding: 20px;
+	.note {
+		padding-top: 20px;
 	}
 </style>
