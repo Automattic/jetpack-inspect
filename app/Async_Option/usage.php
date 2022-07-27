0f9969e5c50928ddb6ee1bd5412e49c03ed4cf22
Nauris Pūķis <hi@pyronaur.com>
2022-07-27 10:34:44 +0300

    Remove commented code

--- app/Async_Option/usage.php
+++ app/Async_Option/usage.php
@@ -16,32 +16,3 @@ $endpoint = new Endpoint( 'jetpack-inspect', $monitor_status );
 add_action('rest_api_init', [$endpoint, 'register_rest_route']);
 
 //$registry->add( $monitor_status );
-
-//Async_Option::setup( 'monitor-filter' )
-//			->validate_with( 'is_string', "The 'name' parameter must be a string." )
-//			->valid_values( Monitors::names() )
-//			->sanitize_with( 'sanitize_text_field' )
-//			->get_value_with( 'get_option' )
-//			->set_value_with( 'update_option' )
-//			->default( 'enabled' )
-//			->on_save( 'strtolower' )
-//			->on_get( 'strtolower' );
-//
-//Async_Option::setup(['remote_request_filter', 'remote_'])
-//			->validate_with( 'is_string', "The 'name' parameter must be a string." )
-//			->valid_values( Monitors::names() )
-//			->sanitize_with( 'sanitize_text_field' )
-//			->get_value_with( 'get_option' )
-//			->set_value_with( 'update_option' )
-//			->default( 'enabled' )
-//			->on_save( 'strtolower' )
-//			->on_get( 'strtolower' );
-//
-//Async_Option::setup('dismissed_notices')
-//			->validate_with( 'is_array', "The 'dismissed_notices' parameter must be an array." )
-//			->
-//			->get_value_with( 'get_option' )
-//			->set_value_with( 'update_option' )
-//			->default( 'enabled' )
-//			->on_save( 'strtolower' )
-//			->on_get( 'strtolower' );
