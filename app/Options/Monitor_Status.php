cee4f840d79072e853b38b7b568787a944efcba3
Nauris Pūķis <hi@pyronaur.com>
2022-07-27 18:11:36 +0300

    Add parse support to Monitor_Status option

--- app/Options/Monitor_Status.php
+++ app/Options/Monitor_Status.php
@@ -2,11 +2,12 @@
 
 namespace Automattic\Jetpack_Inspect\Options;
 
+use Automattic\Jetpack_Inspect\Async_Option\Contracts\Parser;
 use Automattic\Jetpack_Inspect\Async_Option\Contracts\Sanitizer;
 use Automattic\Jetpack_Inspect\Async_Option\Contracts\Transformer;
 use Automattic\Jetpack_Inspect\Async_Option\Contracts\Validator;
 
-class Monitor_Status implements Validator, Sanitizer, Transformer {
+class Monitor_Status implements Validator, Sanitizer, Transformer, Parser {
 
 	public function sanitize( $value ): bool {
 		return (bool) $value;
@@ -14,14 +15,17 @@ class Monitor_Status implements Validator, Sanitizer, Transformer {
 
 	public function validate( $value ) {
 		if ( ! is_bool( $value ) ) {
-			return esc_html__( 'Monitor status should be a boolean.', 'jetpack-inspect' );
+			return sprintf( esc_html__( 'Monitor status should be a boolean. Received "%s". ', 'jetpack-inspect' ), gettype( $value ) );
 		}
-
 		return true;
 	}
 
 	public function transform( $value ) {
 		return (bool) $value;
+	}
+
+	public function parse( $value ) {
+		return filter_var( $value, FILTER_VALIDATE_BOOLEAN );
 	}
 }
 
