5e0b2ce6a5d2353588dcbc84f5c99b57e24996fb
Nauris Pūķis <hi@pyronaur.com>
2022-07-28 10:55:43 +0300

    Implement option registry with helper functions

--- app/Async_Option/usage.php
+++ app/Async_Option/usage.php
@@ -2,17 +2,35 @@
 
 namespace Automattic\Jetpack_Inspect\Async_Option;
 
-use Automattic\Jetpack_Inspect\Async_Option\Storage\WP_Option;
 use Automattic\Jetpack_Inspect\Options\Monitor_Status;
 
-//$registry = new Registry();
 
-$monitor_status = new Async_Option('monitor-status');
-$monitor_status
-	->store( new WP_Option() )
-	->handler( new Monitor_Status() );
+function jetpack_inspect_register_option( $name, $handler = null ) {
+	return Registry::get_instance( 'jetpack_inspect' )
+	               ->regsiter( $name, $handler );
+}
 
-$endpoint = new Endpoint( 'jetpack-inspect', $monitor_status );
-add_action('rest_api_init', [$endpoint, 'register_rest_route']);
 
-//$registry->add( $monitor_status );
+
+/**
+ * Functions to make it easier to interface with Async Option:
+ */
+/**
+ * @param $name
+ *
+ * @return Async_Option
+ */
+function jetpack_inspect_option( $name ) {
+	return Registry::get_instance( 'jetpack_inspect' )->get_option( $name );
+}
+
+function jetpack_inspect_get_option( $option ) {
+	return jetpack_inspect_option( $option )->get();
+}
+
+function jetpack_inspect_update_option( $option, $value ) {
+	return jetpack_inspect_option( $option )->set( $value );
+}
+
+
+jetpack_inspect_register_option( 'monitor_status', Monitor_Status::class );
