fe2f065af83fe7a2f102c0b975a9b995fe321691
Nauris Pūķis <hi@pyronaur.com>
2022-08-02 17:53:58 +0300

    Move Options out of the tmp file

--- app-ui/src/utils/Async_Options.ts
+++ app-ui/src/utils/Async_Options.ts
@@ -1,24 +1,8 @@
-import { writable } from 'svelte/store';
-import type { Writable } from "svelte/store";
 import { z } from 'zod';
 import API from './API';
-
-interface PendingStore {
-	subscribe: Writable<boolean>["subscribe"];
-	stop: () => void;
-	start: () => void;
-}
+import { Options } from './AsyncOptions/Options';
 
 
-
-function createPendingStore(): PendingStore {
-	const { set, subscribe } = writable(false);
-	return {
-		subscribe,
-		stop: () => set(false),
-		start: () => set(true),
-	}
-}
 
 function getValidatedOptions<T extends z.ZodTypeAny>(parser: T, key: string): z.infer<T> {
 	let options = {};
@@ -39,85 +23,6 @@ function getValidatedOptions<T extends z.ZodTypeAny>(parser: T, key: string): z.
 
 	return parsed.data;
 }
-
-
-type OptionShape = {
-	[key: string]: {
-		value: any,
-		nonce: string,
-	}
-}
-
-interface AsyncStore<T> {
-	store: Writable<T>
-	pending: PendingStore;
-}
-
-class Options<T extends OptionShape> {
-	private options: T;
-
-	constructor(namespace: string, options: T) {
-		this.options = options;
-	}
-
-	value<K extends keyof T>(key: K): T[K]["value"] {
-		return this.options[key].value;
-	}
-
-	createStore<K extends keyof T>(key: K, updateCallback: (value: T[K]) => Promise<T[K]["value"]>): AsyncStore<T[K]["value"]> {
-
-		const store = writable(this.value(key));
-		const pending = createPendingStore();
-
-		let requestLock = false;
-		let debounce = 0;
-
-		// Sync the value to the API
-		// And make sure that the value
-		// hasn't changed since it was last submitted.
-		const send = async (value: T[K]["value"]) => {
-
-			// Prevent multiple requests from being sent at once.
-			if (requestLock) {
-				return;
-			}
-
-			// If UI Changes rapidly, wait for it to settle before issuing the request.
-			if (debounce) {
-				clearTimeout(debounce);
-			}
-
-			// Sync the setting to the server
-			debounce = setTimeout(async () => {
-				requestLock = true;
-				let result = await updateCallback({
-					...this.options[key],
-					value,
-				});
-				requestLock = false;
-
-				// Ensure that the value returned is reflected in the UI.
-				if (result !== value) {
-					store.update(() => result);
-				}
-				pending.stop();
-			}, 200);
-		}
-
-		// Send the store value to the API
-		store.set = (value: T[K]["value"]) => {
-			pending.start();
-			store.update(() => value);
-			send(value);
-		}
-
-		return {
-			store: store,
-			pending: pending,
-		};
-	}
-}
-
 
 
 const OptionValidator = z.object({
