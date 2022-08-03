5845078ac6540d2fe32ae8953cd4556f9781276c
Nauris Pūķis <hi@pyronaur.com>
2022-08-03 12:11:42 +0300

    Fix missing parser on Unsafe_Handler

--- app/Async_Option/Unsafe_Handler.php
+++ app/Async_Option/Unsafe_Handler.php
@@ -2,11 +2,12 @@
 
 namespace Automattic\Jetpack_Inspect\Async_Option;
 
+use Automattic\Jetpack_Inspect\Async_Option\Contracts\Parser;
 use Automattic\Jetpack_Inspect\Async_Option\Contracts\Sanitizer;
 use Automattic\Jetpack_Inspect\Async_Option\Contracts\Transformer;
 use Automattic\Jetpack_Inspect\Async_Option\Contracts\Validator;
 
-class Unsafe_Handler implements Transformer, Validator, Sanitizer {
+class Unsafe_Handler implements Parser, Transformer, Validator, Sanitizer {
 
 	public function transform( $value ) {
 		return $value;
