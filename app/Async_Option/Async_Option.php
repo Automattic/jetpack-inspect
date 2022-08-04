04aac14ee5b7e821fe2c7fdd7d54ba43dc535ef9
Nauris Pūķis <hi@pyronaur.com>
2022-08-04 17:55:38 +0300

    value -> option

--- app/Async_Option/Async_Option.php
+++ app/Async_Option/Async_Option.php
@@ -12,8 +12,6 @@ class Async_Option {
 	 */
 	private $key;
 
-
-
 	/**
 	 * @var Storage
 	 */
@@ -22,9 +20,9 @@ class Async_Option {
 	/**
 	 * @var Async_Option_Template
 	 */
-	protected $value;
+	protected $option;
 
-	
+
 	/**
 	 * @param $namespace string
 	 * @param $key       string
@@ -32,26 +30,26 @@ class Async_Option {
 	 */
 	public function __construct( $namespace, $key, $value ) {
 		$this->key     = $key;
-		$this->value   = $value;
-		$this->storage = $this->value->setup_storage( $namespace );
+		$this->option  = $value;
+		$this->storage = $this->option->setup_storage( $namespace );
 	}
 
 	public function get() {
-		return $this->value->transform(
-			$this->storage->get( $this->key, $this->value::DEFAULT )
+		return $this->option->transform(
+			$this->storage->get( $this->key, $this->option::DEFAULT )
 		);
 	}
 
 	public function set( $input ) {
 
-		$value = $this->value->parse( $input );
+		$value = $this->option->parse( $input );
 
-		if ( true !== $this->value->validate( $value ) ) {
-			return $this->value->get_errors();
+		if ( true !== $this->option->validate( $value ) ) {
+			return $this->option->get_errors();
 		}
 
 		if ( ! empty( $this->storage ) ) {
-			return $this->storage->set( $this->key, $this->value->sanitize( $value ) );
+			return $this->storage->set( $this->key, $this->option->sanitize( $value ) );
 		}
 
 		return false;
@@ -66,11 +64,11 @@ class Async_Option {
 	}
 
 	public function has_errors() {
-		return $this->value->has_errors();
+		return $this->option->has_errors();
 	}
 
 	public function get_errors() {
-		return $this->value->get_errors();
+		return $this->option->get_errors();
 	}
 
 
