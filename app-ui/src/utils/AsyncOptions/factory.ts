a3e62b02e0f4ccc428d43dd58ec82f85d0f5fbc4
Nauris Pūķis <hi@pyronaur.com>
2022-08-09 16:23:45 +0300

    Specify that the K is a string

--- app-ui/src/utils/AsyncOptions/factory.ts
+++ app-ui/src/utils/AsyncOptions/factory.ts
@@ -3,7 +3,7 @@ import type AsyncAPI from './AsyncAPI';
 import type { Options } from './Options';
 
 export function asyncOptionFactory<T extends AO.Options>(options: Options<T>, api: AsyncAPI) {
-	return function <K extends keyof T>(name: K) {
+	return function <K extends keyof T & string>(name: K) {
 		const endpoint = name.replace("_", "-");
 		return options.createStore(name, async ({ value, nonce }) => {
 			return await api.POST<typeof value>(endpoint, nonce, value);
