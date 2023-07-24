81f9ebaf8f66df1cb17f815c674b2337231ec798
gogdzl <gogdzl@users.noreply.github.com>
2023-07-24 14:08:33 +0000

    CRM: Port back 6.1.0 release changelog, readme and package updates (#32028)
    * Release changes
    
    * Update core version and wp_tested version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5645612270

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -55,11 +55,11 @@ return array(
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/ManifestGenerator.php'
 	),
 	'Automattic\\Jetpack\\Composer\\Manager' => array(
-		'version' => '1.1.12.0-alpha1689966284',
+		'version' => '1.1.12.0',
 		'path'    => $vendorDir . '/automattic/jetpack-composer-plugin/src/class-manager.php'
 	),
 	'Automattic\\Jetpack\\Composer\\Plugin' => array(
-		'version' => '1.1.12.0-alpha1689966284',
+		'version' => '1.1.12.0',
 		'path'    => $vendorDir . '/automattic/jetpack-composer-plugin/src/class-plugin.php'
 	),
 	'Automattic\\Jetpack\\Config' => array(
