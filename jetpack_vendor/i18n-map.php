5feadccfc7155f386f0436d7d2e6788fda0da2c0
sdixon194 <sdixon194@users.noreply.github.com>
2023-08-24 15:51:44 +0000

    Backport social 2.2.0, backup 2.1 Changes (#32648)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5965753165

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.21-alpha1692641735',
+      'ver' => '0.2.21',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.18.9-alpha1692641735',
+      'ver' => '1.18.9',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.57.1-alpha1692651329',
+      'ver' => '1.57.1',
     ),
   ),
 );
