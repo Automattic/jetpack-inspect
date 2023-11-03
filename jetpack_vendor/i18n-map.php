d59a6e9ead163fd3f2a84aad478c8eaffa8b9e1a
ouikhuan <ouikhuan@users.noreply.github.com>
2023-11-03 09:09:13 +0000

    Update Move to WordPress.com plugin to 1.0.1 (#33946)
    * Release changelog and update readme for migration plugin
    
    * Run tool fixup project versions
    
    * Cleanup readme and changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6743020293

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.18.14-alpha1698918897',
+      'ver' => '1.18.14',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.58.3-alpha1698929608',
+      'ver' => '1.58.3',
     ),
   ),
 );
