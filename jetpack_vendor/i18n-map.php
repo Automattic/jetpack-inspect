20ed304a37490c92f26f07964ad240f0a6cc75e3
ice9js <ice9js@users.noreply.github.com>
2024-01-04 16:42:51 +0000

    Backport mu-wpcom-plugin 2.0.11, jetpack 13.0-a.13 changes (#34852)
    * Changelog and readme.txt edits.
    
    * Fixup project versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7412164902

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.0.4-alpha1704298857',
+      'ver' => '2.0.4',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.1.1-alpha1704298857',
+      'ver' => '2.1.1',
     ),
   ),
 );
