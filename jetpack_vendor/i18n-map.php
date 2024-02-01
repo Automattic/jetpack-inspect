2ff796f94301019fa2ad77084ab5509a6b5897a2
anomiex <anomiex@users.noreply.github.com>
2024-02-01 19:26:47 +0000

    Update wordpress monorepo (#35384)
    * Update wordpress monorepo
    * Apparently GB changed something that eliminated some duplicate class names
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7746415821

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.0-alpha1706803348',
+      'ver' => '2.1.0-alpha1706814673',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.3.0-alpha1706725881',
+      'ver' => '2.3.0-alpha1706814673',
     ),
   ),
 );
