3c5ed15f7b2f829895740479cce1184d8131a99f
samiff <samiff@users.noreply.github.com>
2024-02-05 18:41:50 +0000

    Backport mu-wpcom-plugin 2.0.18, jetpack 13.1-beta Changes (#35448)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    * Init new cycle
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7789159812

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.0-alpha1706814673',
+      'ver' => '2.1.0',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.3.0-alpha1707116550',
+      'ver' => '2.3.0',
     ),
   ),
 );
