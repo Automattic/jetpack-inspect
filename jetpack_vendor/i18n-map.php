12eaabcc1df747cf4b5797915ab753295d47da97
anomiex <anomiex@users.noreply.github.com>
2024-03-11 15:04:20 +0000

    Update wordpress monorepo (#36325)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8234857804

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.2',
+      'ver' => '2.1.3-alpha1710168606',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.3.4',
+      'ver' => '2.3.5-alpha1710168606',
     ),
   ),
 );
