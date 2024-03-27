54a73672cc900988c4b7fddb803da6b868662480
zinigor <zinigor@users.noreply.github.com>
2024-03-27 11:27:44 +0000

    Backport mu-wpcom-plugin 2.1.10, jetpack 13.3-a.9 Changes (#36606)
    * Changelog and readme.txt edits.
    
    * Init new cycle
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8450840615

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.6-alpha1711470495',
+      'ver' => '2.1.6',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.7.0-alpha1711470495',
+      'ver' => '2.7.0',
     ),
   ),
 );
