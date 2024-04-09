cb83feb7a0e79a66b47edea72f9266dc76f6f65a
anomiex <anomiex@users.noreply.github.com>
2024-04-09 15:41:57 +0000

    Backport mu-wpcom-plugin 2.1.14, jetpack 13.4-a.1 Changes (#36792)
    Changelog and readme.txt edits.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8618336925

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.7.1-alpha1712563319',
+      'ver' => '2.7.1',
     ),
   ),
 );
