082be69bf97cb216a8d2c83ab24b515feb9743d8
zinigor <zinigor@users.noreply.github.com>
2024-08-26 15:58:07 +0000

    Backport mu-wpcom-plugin 2.5.9, jetpack 13.8-a.7, wpcomsh 5.6.1 Changes (#39080)
    * Changelog and readme.txt edits.
    
    * Init new cycle
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10562757597
    
    Upstream-Ref: Automattic/jetpack@abb0eda7c453c99c676c2cecc9f84136ed99143a

--- vendor/jetpack-autoloader/class-version-selector.php
+++ vendor/jetpack-autoloader/class-version-selector.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp2662d159b2161b86f56c5059a9d397af_inspectⓥ0_1_0_alpha\al3_0_10_alpha;
+namespace Automattic\Jetpack\Autoloader\jp2662d159b2161b86f56c5059a9d397af_inspectⓥ0_1_0_alpha\al3_0_10;
 
  // phpcs:ignore
 
