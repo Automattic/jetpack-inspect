58c4532ce3562066e42cca92d58715f92eaec9ca
kraftbj <kraftbj@users.noreply.github.com>
2024-09-06 16:27:36 +0000

    Changelog and readme.txt edits. (#39273)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10741662785
    
    Upstream-Ref: Automattic/jetpack@b76b00d65a740df4159322d68d0a20317dbd5a1e

--- vendor/jetpack-autoloader/class-latest-autoloader-guard.php
+++ vendor/jetpack-autoloader/class-latest-autoloader-guard.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp2662d159b2161b86f56c5059a9d397af_inspectⓥ0_1_0_alpha\al3_1_0_alpha;
+namespace Automattic\Jetpack\Autoloader\jp2662d159b2161b86f56c5059a9d397af_inspectⓥ0_1_0_alpha\al3_1_0;
 
  // phpcs:ignore
 
@@ -93,7 +93,7 @@ class Latest_Autoloader_Guard {
 	 *
 	 * Called from the plugins_loaded hook.
 	 *
-	 * @since 3.1.0-alpha
+	 * @since 3.1.0
 	 * @return void
 	 */
 	public function check_for_conflicting_autoloaders() {
