77fa352243e35066d5a10089aa75720ee649517a
ice9js <ice9js@users.noreply.github.com>
2024-09-23 20:22:41 +0000

    Changelog and readme.txt edits. (#39497)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11001614719
    
    Upstream-Ref: Automattic/jetpack@89f781260f91d4acff0467d33b4a8242a7f2de37

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '5.0.0-alpha1727102804',
+      'ver' => '5.0.0',
     ),
   ),
 );
