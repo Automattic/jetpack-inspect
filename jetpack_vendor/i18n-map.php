c7cd99383e9ef4e18f23a531ee19b61255fc4394
zinigor <zinigor@users.noreply.github.com>
2024-11-25 17:48:33 +0000

    Changelog and readme.txt edits. (#40333)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12015553649
    
    Upstream-Ref: Automattic/jetpack@e34d1bcfbd7cafe30ca907681c9de5830df047c0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.1-alpha1732217150',
+      'ver' => '0.5.1',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.0-alpha1732205217',
+      'ver' => '4.0.0',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.1.0-alpha1732217150',
+      'ver' => '6.1.0',
     ),
   ),
 );
