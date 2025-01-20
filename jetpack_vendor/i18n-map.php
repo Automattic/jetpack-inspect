054e22737fbdea25fe5327a56cbc56d5158d7efe
tbradsha <tbradsha@users.noreply.github.com>
2025-01-20 15:55:26 +0000

    Phan: functionify and statusify `exit()` and `die()` (#41167)
    * non-CRM: exit → exit( 0 )
    
    * CRM: exit → exit( 0 )
    
    * non-CRM: exit() → exit( 0 )
    
    * CRM: exit() → exit( 0 )
    
    * die → die(0)
    
    * Suppress invalid Phan error on valid tests
    
    * die() → die(0)
    
    * Add changelogs
    
    * Suppress unneeded suppression warnings on PHP 8.2
    
    * Oops
    
    * Rework code to satisfy Phan bug
    
    * Remove redundant exits
    
    * Correct PHPDoc
    
    * Update baseline
    
    * Remove comments
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12871434613
    
    Upstream-Ref: Automattic/jetpack@d2c7b02f9ccf51c7390e0aa36ae1f520377c2108

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.3.0-alpha1737371572',
+      'ver' => '6.3.0-alpha1737387586',
     ),
   ),
 );
