a9698dc2f3379a8c5d18de07d9dc31b6e11e397e
anomiex <anomiex@users.noreply.github.com>
2025-03-03 18:33:48 +0000

    phpunit: Rename package test classes for PHPUnit 11 compat (pt 1) (#42120)
    PHPUnit 10+ requires that test class names match the filename, PSR-4
    style. This PR renames the test classes and files in packages to end in
    `Test` or `_Test`, depending on whether the class names are following
    WordPress's underscore-naming convention or are using CamelCase, and
    removing redundant `WP_Test_` or `Test_` prefixes.
    
    This also adds our standard Jetpack-Tests ruleset to
    tests/.phpcs.dir.xml in the packages, and removes some redundant inline
    phpcs:ignore or phpcs:disable directives.
    
    Of note, changed test order in the Connection package broke a test.
    Adding some setup/teardown code to clear the affected singleton fixed
    it.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13637614579
    
    Upstream-Ref: Automattic/jetpack@327f25f4acf9a71d70b3f6883f26acc3377365a5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.3',
+      'ver' => '0.5.4-alpha1741025873',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.9-alpha1741019398',
+      'ver' => '4.0.9-alpha1741025873',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.7.0-alpha1741019398',
+      'ver' => '6.7.0-alpha1741025873',
     ),
   ),
 );
