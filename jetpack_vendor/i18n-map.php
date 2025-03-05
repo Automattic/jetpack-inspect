9e23ea2097708f6c94b61659a53775017d2cb272
dilirity <dilirity@users.noreply.github.com>
2025-03-05 13:14:20 +0000

    Changelog and readme.txt edits. (#42222)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13676530376
    
    Upstream-Ref: Automattic/jetpack@aff374a8994ab73af6f069899768be29812ec0e6

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.4-alpha1741025873',
+      'ver' => '0.5.4',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.10-alpha1741026079',
+      'ver' => '4.0.10',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.7.1-alpha1741026079',
+      'ver' => '6.7.1',
     ),
   ),
 );
