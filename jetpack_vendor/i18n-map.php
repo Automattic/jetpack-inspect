50d4816186385b9c28cabb5edbb0f8b5bdba13a1
anomiex <anomiex@users.noreply.github.com>
2025-03-17 14:57:25 +0000

    phpunit: Static-ify data providers (#42484)
    PHPUnit 11 requires that all `@dataProvider` functions are `static`.
    Most of the time this is straightforward, as most of the time data
    providers return a static array of test cases.
    
    `WafRuntimeTargetsTest` was a major exception: it's building
    `Waf_Runtime` objects for each case, often with mocks, and PHPUnit
    doesn't provide methods to create mocks statically. What I wound up
    having to do there was return a factory method instead, which would take
    `$this` as a parameter to use to create the mocks.
    
    Jetpack also had a few that were somehow passing despite trying to use
    `$this->var` before that got set by `set_up()`. And a couple of data
    providers taking an unused `$name` parameter that is no longer passed.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13902180586
    
    Upstream-Ref: Automattic/jetpack@601cf65b9abedbe320a48b03ee9ca6edeb73e40e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.5',
+      'ver' => '0.5.6-alpha1742222219',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.11',
+      'ver' => '4.0.12-alpha1742222219',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.7.4',
+      'ver' => '6.7.5-alpha1742222219',
     ),
   ),
 );
