f806239b6b04d515d04afc7a9c8050fcec3cd830
anomiex <anomiex@users.noreply.github.com>
2025-03-26 14:37:39 +0000

    phpunit: Remove unnecessary uses of `@before`, `@after`, etc (#42706)
    Back when we still supported PHP <=7.1, we had to add these in a lot of
    places to work around the fact that PHPUnit 8 started declaring a `void`
    return type on `setUp()`, `tearDown()`, and so on, which 7.1 and earler
    don't support.
    
    But we've since dropped support for PHP <7.2, so we can swtich back to
    the normal functions. It's more straightforward to do that than to start
    adding attributes (`#[Before]` and such) for PHPUnit 11+ where the
    annotations are deprecated.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14085626545
    
    Upstream-Ref: Automattic/jetpack@617a716856cc1f098554c281bb51f670814aaecf

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.14',
+      'ver' => '4.0.15-alpha1742998768',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.9.0',
+      'ver' => '6.9.1-alpha1742998768',
     ),
   ),
 );
