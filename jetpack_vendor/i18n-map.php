acc56d89f13736ba2c1d60fdefac6b1fa7dbadfc
tbradsha <tbradsha@users.noreply.github.com>
2025-06-03 18:11:39 +0000

    Backport package release cycle for 2025-06 (#43751)
    * js-packages/analytics
    
    * js-packages/api
    
    * js-packages/base-styles
    
    * js-packages/boost-score-api
    
    * js-packages/components
    
    * js-packages/config
    
    * js-packages/connection
    
    * js-packages/idc
    
    * js-packages/image-guide
    
    * js-packages/licensing
    
    * js-packages/partner-coupon
    
    * js-packages/publicize-components
    
    * js-packages/scan
    
    * js-packages/shared-extension-utils
    
    * js-packages/storybook
    
    * js-packages/babel-plugin-replace-textdomain 1.0.46
    
    * js-packages/i18n-check-webpack-plugin 1.1.20
    
    * js-packages/number-formatters 1.0.3
    
    * js-packages/social-logos 3.2.1
    
    * js-packages/webpack-config 3.6.16
    
    * packages/assets 4.0.24
    
    * packages/blaze 0.25.28
    
    * packages/calypsoify 0.2.4
    
    * packages/classic-theme-helper 0.13.4
    
    * packages/connection 6.11.9
    
    * packages/google-analytics 0.3.2
    
    * packages/forms 1.0.0
    
    * packages/jetpack-mu-wpcom 6.4.0
    
    * packages/jitm 4.2.20
    
    * packages/masterbar 0.17.6
    
    * packages/scheduled-updates 0.14.2
    
    * packages/subscribers-dashboard 0.1.14
    
    * packages/sync 4.14.1
    
    * packages/transport-helper 0.3.2
    
    * packages/yoast-promo 0.3.3
    
    * Remaining version bump files
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15424390011
    
    Upstream-Ref: Automattic/jetpack@9806104b3dce1f71bce7d69cc0251a0732ce23f0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.23',
+      'ver' => '4.0.24',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.9-alpha1748892771',
+      'ver' => '6.11.9',
     ),
   ),
 );
