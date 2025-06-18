bb7fd33e8c3913b02d4fd63f17ba32571fc336d2
tbradsha <tbradsha@users.noreply.github.com>
2025-06-18 15:37:48 +0000

    Changelog and readme.txt edits. (#44002)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15736927987
    
    Upstream-Ref: Automattic/jetpack@36b1e75c66d04ef3686a0ceab7f3745706aa4d4d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.27-alpha1750107007',
+      'ver' => '4.0.27',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.4',
+      'ver' => '6.13.5',
     ),
   ),
 );
