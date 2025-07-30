1c0e70307a1dadad82e015ca0c7c113420e8e3e2
anomiex <anomiex@users.noreply.github.com>
2025-07-30 20:32:29 +0000

    eslint: Add eslint-plugin-package-json (#44559)
    This eslint plugin will sort package.json files and apply other
    cleanups, replacing the manual use of `sort-package-json` which we're
    currently only applying to the monorepo root file.
    
    The configuration is slightly complex, in that we want different
    treatment for files in different places:
    * Only js-packages require a `name` field.
    * Only public js-packages require a description or version.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16632823177
    
    Upstream-Ref: Automattic/jetpack@ec35e7c361131766cc40933e2c2dad8a6df45d34

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.10',
+      'ver' => '0.5.11-alpha1753906593',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.1',
+      'ver' => '4.3.2-alpha1753906593',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.16.2',
+      'ver' => '6.16.3-alpha1753906593',
     ),
   ),
 );
