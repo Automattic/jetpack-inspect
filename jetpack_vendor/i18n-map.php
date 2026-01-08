1256fb539c5d2b8fb1d0921c4edd9ac43b27010c
dilirity <dilirity@users.noreply.github.com>
2026-01-08 10:43:24 +0000

    Backport boost 4.5.5-beta changes (#46474)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20813623657
    
    Upstream-Ref: Automattic/jetpack@90810b4e9fd89dfaab6d6dec50e4897eebd6bbfc

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.19-alpha1767721690',
+      'ver' => '4.3.19',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.20.0-alpha1767721690',
+      'ver' => '6.20.0',
     ),
   ),
 );
