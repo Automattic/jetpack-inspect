3d3136cb1cc382f56a8913d49bb64e742c5df5eb
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-01-26 10:15:28 +0000

    Backport jetpack 15.5-a.5 changes (#46758)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21353487016
    
    Upstream-Ref: Automattic/jetpack@5987b386be2e301d736a49c92b5e0aa0797d2c57

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.20',
+      'ver' => '4.3.21',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.20.4',
+      'ver' => '6.20.5',
     ),
   ),
 );
