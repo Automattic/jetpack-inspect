cf3051760310bb6c280ee42385d52dfe0319cb3e
sdnunca <sdnunca@users.noreply.github.com>
2026-03-09 10:39:41 +0000

    Backport boost 4.5.8-beta changes (#47508)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Normalize changelogs
    
    ---------
    
    Co-authored-by: Gergely Juhasz <gergely.juhasz@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22848836734
    
    Upstream-Ref: Automattic/jetpack@20ecdd4698f4232f33a573b3affa25e825b6cec5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.12-alpha1772528454',
+      'ver' => '0.5.12',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.26',
+      'ver' => '4.3.27',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.0.3-alpha1772963099',
+      'ver' => '8.0.3',
     ),
   ),
 );
