de59cda63edd4b6bf3e3cc1fec80922f29a8d496
tbradsha <tbradsha@users.noreply.github.com>
2026-04-27 20:59:59 +0000

    Tooling: Remove changelogger as dependency from all projects (#48225)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25018520105
    
    Upstream-Ref: Automattic/jetpack@ed75a01208fb6795ca774b10d19a8e984d844c0b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,19 +6,19 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.1',
+      'ver' => '0.8.2-alpha1777322605',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.33',
+      'ver' => '4.3.34-alpha1777322605',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '3.1.1',
+      'ver' => '3.1.2-alpha1777322605',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.3-alpha1777311482',
+      'ver' => '8.3.3-alpha1777322605',
     ),
   ),
 );
