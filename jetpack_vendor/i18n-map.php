55a591342db8c018b40df64afbd29ebc426f6976
tbradsha <tbradsha@users.noreply.github.com>
2026-05-14 21:31:39 +0000

    Phan: Address PhanPluginSimplifyExpressionBool violations (#48810)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25886131945
    
    Upstream-Ref: Automattic/jetpack@ca05f2ff95509756c7f738586355c6d182dea5eb

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.6-alpha1778782620',
+      'ver' => '8.3.6-alpha1778793320',
     ),
   ),
 );
