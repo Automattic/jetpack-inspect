6bf72677ebb0c4bd70621d2cf503cf9938641a72
obenland <obenland@users.noreply.github.com>
2026-05-14 16:48:26 +0000

    Backport videopress 3.1 changes (#48834)
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25872098107
    
    Upstream-Ref: Automattic/jetpack@950ea11562bd4cfa2af38d525046cfe4825d5d03

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.4-alpha1778690619',
+      'ver' => '0.8.4',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.5-alpha1778770251',
+      'ver' => '8.3.5',
     ),
   ),
 );
