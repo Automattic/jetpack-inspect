940c129f9d37befe08b3e5cad7137a2d0f125e60
tbradsha <tbradsha@users.noreply.github.com>
2026-06-02 20:09:58 +0000

    Tests: Split code coverage scripts into test-js-coverage and test-php-coverage (#48845)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26844277266
    
    Upstream-Ref: Automattic/jetpack@b25e59181090ffcd52db1200e882dbb26ebc543e

--- vendor/jetpack-autoloader/class-autoloader-handler.php
+++ vendor/jetpack-autoloader/class-autoloader-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp2662d159b2161b86f56c5059a9d397af_inspectⓥ2_0_0_alpha\al5_0_18;
+namespace Automattic\Jetpack\Autoloader\jp2662d159b2161b86f56c5059a9d397af_inspectⓥ2_0_0_alpha\al5_0_19_alpha;
 
  // phpcs:ignore
 
