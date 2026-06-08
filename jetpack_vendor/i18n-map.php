52ee10c76a133c7a06a3131b2b5c9e5dd8182738
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2026-06-08 09:17:46 +0000

    Backport boost 4.6.0-beta changes (#49450)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27126919399
    
    Upstream-Ref: Automattic/jetpack@fc06f55557f03c9c1b5107bb559f2ffaff017208

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.1-alpha1780671753',
+      'ver' => '0.9.1',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.41-alpha1780671753',
+      'ver' => '4.3.41',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.5-alpha1780671753',
+      'ver' => '8.5.5',
     ),
   ),
 );
