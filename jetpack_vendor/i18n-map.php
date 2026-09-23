b32fbf679acce8bcf1d2121ba7228933dea1ac8c
Nikschavan <Nikschavan@users.noreply.github.com>
2026-09-23 09:39:52 +0000

    Backport stats 1.0.0 changes (#52691)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35842276649
    
    Upstream-Ref: Automattic/jetpack@724baca6b99e60a1ed50bede6e14656891f889ce

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '5.0.5',
+      'ver' => '5.0.6',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,11 +18,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.6.0-alpha1790154608',
+      'ver' => '9.7.0-alpha1790155179',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.7.0-alpha1790143860',
+      'ver' => '0.7.0',
     ),
   ),
   'paths' => array(
